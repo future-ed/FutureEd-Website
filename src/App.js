f850eb9e76897ad3ec139c039e1ee47ad2352e10
Sunny <sunnynate@hotmail.com>
2023-03-07 22:11:31 +0100

    Change webapp title and icon

--- src/App.js
+++ src/App.js
@@ -4,17 +4,25 @@ import Home from './pages/Home_page';
 import Products from './pages/Products_page';
 import Company from './pages/Company_page';
 import Support from './pages/Support_page';
+import Helmet from 'react-helmet';
 
 function App() {
   return (
-    <BrowserRouter>	
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="products" element={<Products />} />
-        <Route path="company" element={<Company />} />
-        <Route path="support" element={<Support />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>FutureEd</title>
+      </Helmet>
+
+      <BrowserRouter>	
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="products" element={<Products />} />
+          <Route path="company" element={<Company />} />
+          <Route path="support" element={<Support />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 

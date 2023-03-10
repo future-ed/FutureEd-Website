23c73a819cf77fccce8830f51e6e9789705448b3
Sunny <sunnynate@hotmail.com>
2023-03-10 15:20:54 +0100

    Price adjustment

--- src/components/Products_Abbos.jsx
+++ src/components/Products_Abbos.jsx
@@ -13,24 +13,29 @@ const Abbos = () => {
           <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
               <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Single} alt="/" />
               <h2 className='text-2xl font-bold text-center py-8'>1 persoon</h2>
-              <p className='text-center text-4xl font-bold'>€424.99 pp</p>
+              <p className='text-center text-4xl font-bold'>&lt;€24/uur</p>
               <div className='text-center font-medium'>
                   <p className='py-2 border-b mx-8 mt-8'>Voor studenten</p>
                   <p className='py-2 border-b mx-8'>12 lessen in groep van 4</p>
                   <p className='py-2 border-b mx-8'>6 modules, 20 opdrachten</p>
                   <p className='py-2 border-b mx-8'>Wekelijks 1 online les van 1.5 uur</p>
+                  <p className='py-2 border-b mx-8'>Totaal €424.99 pp</p>
+                  <p className='py-2 border-b mx-8'>Betaling in termijnen mogelijk</p>
               </div>
               <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'><HashLink smooth to='/support/#Contact' >Start</HashLink></button>
           </div>
           <div className='w-full shadow-xl bg-gray-100 flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
               <img className='w-20 mx-auto mt-[-3rem] bg-transparent' src={Double} alt="/" />
               <h2 className='text-2xl font-bold text-center py-8'>Samen met een vriend(in)</h2>
-              <p className='text-center text-4xl font-bold'>€394.99 pp</p>
+              <p className='text-center text-4xl font-bold'>&lt;€22/uur</p>
               <div className='text-center font-medium'>
               <p className='py-2 border-b mx-8 mt-8'>Voor studenten</p>
                   <p className='py-2 border-b mx-8'>12 lessen in groep van 4</p>
                   <p className='py-2 border-b mx-8'>6 modules, 20 opdrachten</p>
                   <p className='py-2 border-b mx-8'>Wekelijks 1 online les van 1.5 uur</p>
+                  <p className='py-2 border-b mx-8'>Totaal €394.99 pp</p>
+                  <p className='py-2 border-b mx-8'>Betaling in termijnen mogelijk</p>
+
               </div>
               <button className='bg-black text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'><HashLink smooth to='/support/#Contact' >Start</HashLink></button>
           </div>
@@ -43,6 +48,8 @@ const Abbos = () => {
                   <p className='py-2 border-b mx-8'>Stof voor een half of heel jaar</p>
                   <p className='py-2 border-b mx-8'>Modules en opdrachten naar wens</p>
                   <p className='py-2 border-b mx-8'>Vormgeving naar wens</p>
+                  <p className='py-2 border-b mx-8'>Prijs op aanvraag</p>
+                  <p className='py-2 border-b mx-8'>Betaling in termijnen mogelijk</p>
               </div>
               <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'><HashLink smooth to='/support/#Contact' >Meer info</HashLink></button>
           </div>

abd0c701409f8c5ddcfeb32ba4f6a0cf489fb3a7
shcl1999 <sunnynate@hotmail.com>
2023-12-07 19:39:37 +0100

    added tax I subchapter

--- src/components/Account/Abstracts/Taxes/TaxesI.jsx
+++ src/components/Account/Abstracts/Taxes/TaxesI.jsx
@@ -1,25 +1,19 @@
 import React from 'react'
-import GeneralAbstract from '../General_Abstract/General_Abstract';
-import backgroundImage from '../../../../assets/Personal_Finances.png';
+import c1 from '../../../../assets/tax_i1.svg';
+import c2 from '../../../../assets/tax_i2.svg';
+import c3 from '../../../../assets/tax_i3.svg';
+import GeneralContent from '../General_Abstract/General_Content';
 
 const TaxesI = ({ onBackClick, readStatus, onToggleRead }) => {
-    const content = (
-        <div>
-            <p>• The €57.000 Exemption</p>
-            <p>• How does Capital Tax work</p>
-            <p>• How to Calculate Your Own I Taxes</p>
-            <div className='mt-3'>
-                <p>In this section, we explore the capital tax an investor needs to pay and the €57,000 capital exemption 
-                    you have as an investor. And lastly how students can calculate their own Taxes as an Investor.</p>
-            </div>
-            
-        </div>
-    );
+    const contentPictures = [
+      c1,
+      c2,
+      c3,
+  ]
   return (
-    <GeneralAbstract 
-            backgroundImage={backgroundImage}
+    <GeneralContent 
             title="Taxes I"
-            content={content}
+            content={contentPictures}
             onBackClick={ onBackClick }
             readStatus={readStatus}
             onToggleRead={onToggleRead}
